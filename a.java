acdf01a5f8c33d4967a99c2b7d4f6a387e5a2aad
Vaibhav Keshari <110326184+kesharivaibhav111@users.noreply.github.com>
2025-06-09 10:23:38 +0530

    hello2

--- a.java
+++ a.java
@@ -1,18 +1,18 @@
-// class Alpha {
-//     int d;
+class a {
+    int d;
 
-//     public void setNum(int a)
-//     {
-//         d = a;
-//     }
-//     public int getNum(){
-//         return a;
-//     }
-//     public static void main(String[] args) {
-//         Alpha A = new Alpha();
-//         A.setNum(5);
-//         System.out.println(A.getNum());
+    public void setNum(int a)
+    {
+        d = a;
+    }
+    public int getNum(){
+        return d;
+    }
+    public static void main(String[] args) {
+        a A = new a();
+        A.setNum(5);
+        System.out.println(A.getNum());
 
-//     }
+    }
 
-// }
+}
